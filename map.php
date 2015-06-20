bb2fb82e9cbf4c32399ee59b400417f45c17706c
Hermann Höhne <hermann@hehoe.de>
2015-06-20 17:13:05 +0200

    Hacked in a heatmap-like output.

--- map.php
+++ map.php
@@ -21,23 +21,78 @@ foreach ($requests as $request) {
   usort($stations,"sortByLevel");
   $data[] = array("date"=>$date, "time"=>$time, "phone"=>$phone, "stations"=>$stations, "comment"=>$comment);
 }
+if (isset($_GET["phone"])) {
+  function phone($var) {
+    return $var["phone"] == $_GET["phone"];
+  }
+  $data = array_filter($data, "phone");
+}
+if (!isset($_GET["i"])) {
+  $selected = $data[count($data)-1];
+} else {
+  $selected = $data[$_GET["i"]];
+}
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Map</title>
-	<meta charset="UTF-8" />
+  <title>Map</title>
+  <meta charset="UTF-8" />
+  <style type="text/css">
+      body, html {
+        height:100%;
+        width:100%;
+        margin: 0px;
+        font-family: sans-serif;
+      }
+  </style>
   <script>
+    function level2radius(x) {
+      return 1.09491*(Math.exp(-0.0561178*x)+10);
+    }
     function markAPs() {
-      svgmap = document.getElementsByTagName("svg")[0]
+      var svgmap = document.getElementsByTagName("svg")[0];
+      svgmap.style.height="100%";
       <?php
       // TODO: replace this by in-svg css
-      /*
-        foreach ($bssids as $bssid) {
-          echo "svgmap.getElementById(\"".$bssid."\").style[\"fill\"] = \"red\";\n";
+        $stations = $selected["stations"];
+        $bssids = array();
+        $levels = array();
+        foreach ($stations as $station) {
+          $bssids[] = $station["bssid"];
+          $levels[] = $station["level"];
         }
-        */
+        echo "var bssids = [\"".implode("\", \"",$bssids)."\"];";
+        echo "var levels = [\"".implode("\", \"",$levels)."\"];";
       ?>
+      for (var i = 0; i < bssids.length ; ++i)  {
+        var bssid = bssids[i];
+        var level = parseInt(levels[i]);
+        if (level < -100) {
+          level = -100;
+        }
+        if (level > -30) {
+          level = -30;
+        }
+        var ap = svgmap.getElementById(bssid);
+        if (ap == null) {
+          console.log("unknown bssid "+bssid);
+        } else {
+          console.log("marked bssid "+bssid+" with level "+level);
+          ap.style["fill"] = "red";
+          ap.style["fill-opacity"] = (130 + level)/100.0/2;
+          var radius = level2radius(level);
+          if (ap.tagName == "circle") {
+            ap.r.baseVal.value = radius;
+          }
+          else if (ap.tagName == "rect") {
+            ap.height.baseVal.value = radius*2;
+            ap.width.baseVal.value = radius*2;
+            ap.x.baseVal.value -= radius;
+            ap.y.baseVal.value -= radius;
+          }
+        }
+      }
     }
   </script>
 </head>
@@ -55,6 +110,7 @@ if (false) {
   }
 }
 echo file_get_contents("map.svg");
+echo "\n<br/>".$selected["comment"];
 ?>
 </body>
 </html>
