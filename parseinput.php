618db86c8d0d0e305581b5b6ff6e4e22cfd11e88
Hermann Höhne <inf100314@fh-wedel.de>
2015-06-24 16:36:41 +0200

    fehlende Datei parseinput.php eingecheckt

---
+++ parseinput.php
@@ -0,0 +1,58 @@
+<?php
+function sortByLevel($a, $b){
+  if ($a["level"] == $b["level"]) {
+    return strcmp($a["bssid"],$b["bssid"]);
+  }
+  return ($a["level"] > $b["level"]) ? -1 : 1;
+}
+
+function parse_input() {
+  // parse input
+  $requests = file("requests.log");
+  $data = array();
+  foreach ($requests as $request) {
+    $parts = explode("\t",$request);
+    list ($date, $time, $json) = $parts;
+    $jsonobj = json_decode($json);
+    $phone = $jsonobj->{"phone"};
+    $comment = $jsonobj->{"comment"};
+    $stations = array();
+    if (isset($jsonobj->{"stations"})) {
+      $jsonstations = $jsonobj->{"stations"};
+      if (!is_array($jsonstations)) {
+        $jsonstations = array($jsonstations);
+      }
+      foreach ($jsonstations as $jsonstation) {
+        $bssid = $jsonstation->{"bssid"};
+        $level = intval($jsonstation->{"level"});
+        $stations[] = array("bssid"=>$bssid, "level"=>$level);
+      }
+      usort($stations,"sortByLevel");
+    }
+    $data[] = array("date"=>$date, "time"=>$time, "phone"=>$phone, "stations"=>$stations, "comment"=>$comment);
+  }
+return $data;
+}
+
+function select_data($data) {
+  // filter based on user supplied parameters
+  if (isset($_GET["phone"])) {
+    function phone($var) {
+      return $var["phone"] == $_GET["phone"];
+    }
+    $data = array_filter($data, "phone");
+  }
+  if (!isset($_GET["i"])) {
+    $index = -1;
+  } else {
+    $index = intval($_GET["i"]);
+  }
+  // select data
+  if ($index < 0) {
+    $selected = $data[count($data)+$index];
+  } else {
+    $selected = $data[$index];
+  }
+  return $selected;
+}
+?>
