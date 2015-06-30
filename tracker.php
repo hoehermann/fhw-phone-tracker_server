077511a9a08bc8c00bf22086848609faaf69e0b9
Hermann Höhne <inf100314@fh-wedel.de>
2015-06-30 13:52:14 +0200

    alte Daten werden automatisch verworfen

--- tracker.php
+++ tracker.php
@@ -1,9 +1,10 @@
 <?php
-#echo "Begin";
+$filename = "requests.log";
+if (filesize($filename) > 200000) {
+  rename($filename,$filename.".old");
+}
 $date = date("Y-m-d\tH:i:s");
-$post = file_get_contents('php://input'); #var_export($_POST["name"]);
+$post = file_get_contents('php://input');
 $tofile = $date."\t".$post."\n";
-file_put_contents("requests.log",$tofile,FILE_APPEND|LOCK_EX);
-var_dump($tofile);
-#echo "OK";
+file_put_contents($filename,$tofile,FILE_APPEND|LOCK_EX);
 ?>
