462e184916dd3f8613a8d272ff4ceb0580c3683e
Hermann Höhne <inf100314@fh-wedel.de>
2015-07-08 14:01:31 +0200

    Old requests are actually removed.

--- tracker.php
+++ tracker.php
@@ -2,6 +2,7 @@
 $filename = "requests.log";
 if (filesize($filename) > 200000) {
   file_put_contents($filename.".old",file_get_contents($filename),FILE_APPEND|LOCK_EX);
+  file_put_contents($filename,"",LOCK_EX);
 }
 $date = date("Y-m-d\tH:i:s");
 $post = file_get_contents('php://input');
